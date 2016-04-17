6f2b4c5dea5a892c64bc3fbefd937bd17bf60e3c
Mike Wiesenhart <mike@mdwbkny.com>
2016-04-17 17:04:14 -0400

    Styling changes, starting to integrate Stripe, and moar.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   
   root 'users#home'
   resources :users
+  get 'teams'=> 'users#index'
+  get 'dues' => 'users#dues'
+  get 'trophies' => 'users#trophies'
+  get 'champions' => 'users#champions'
+  get 'sacko' => 'users#sacko'
   
 end
